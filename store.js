4a8ba0db9d82247cb5a6bb89c972e5375ad63b62
lamiarahman678 <lrahman2@pride.hofstra.edu>
2022-05-14 16:01:24 -0400

    Fixed the issue with total and now it updates properly

--- store.js
+++ store.js
@@ -27,12 +27,23 @@ for (var i=0; i<addToCartButtons.length; i++){
     button.addEventListener('click', addToCartClicked)
 }
 
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+
 }
 function removeCartItem(event){
     var buttonClicked =event.target
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
 
+}
+
+function purchaseClicked(){
+    alert('Thank you for your purchase!')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while(cartItems.hasChildNodes()){
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
 }
 
 function quantityChanged(event){
@@ -82,9 +93,9 @@ function addItemToCart(title, price, image){
 function updateCartTotal(){
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
     for(var i=0; i<cartRows.length;i++){
         var cartRow = cartRows[i]
-        var total = 0
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')
         [0]
